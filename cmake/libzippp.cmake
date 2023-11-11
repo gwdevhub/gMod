b51961ee79d1fd7755ca9387cb20d118f98302eb
DubbleClick <m@pyc.ac>
2023-11-11 20:49:02 +0100

    use ExternalProject test (doesnt work maybe?!)

--- cmake/libzippp.cmake
+++ cmake/libzippp.cmake
@@ -2,6 +2,20 @@ include_guard()
 include(FetchContent)
 include(libzip)
 
+include(ExternalProject)
+
+# Download and build libzip as an external project
+ExternalProject_Add(
+    libzip_external
+    GIT_REPOSITORY https://github.com/nih-at/libzip.git
+    GIT_TAG v1.10.1
+    PREFIX ${CMAKE_BINARY_DIR}/libzip
+    CMAKE_ARGS -DLIBZIP_DO_INSTALL=OFF
+)
+
+# Find the libzip package after it's built
+find_package(libzip REQUIRED)
+
 FetchContent_Declare(
     libzippp
     GIT_REPOSITORY https://github.com/ctabin/libzippp
@@ -21,6 +35,7 @@ set(SOURCES
 source_group(TREE ${libzippp_SOURCE_DIR} FILES ${SOURCES})
 target_sources(libzippp PRIVATE ${SOURCES})
 target_include_directories(libzippp PUBLIC "${libzippp_SOURCE_DIR}")
+target_include_directories(libzippp PRIVATE "${libzip_SOURCE_DIR}")
 target_link_libraries(libzippp PRIVATE libzip)
 
 set_target_properties(libzippp PROPERTIES FOLDER "Dependencies/")
