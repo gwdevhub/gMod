5fa4e93b0227b869ec845889441f2fec17d48172
DubbleClick <m@pyc.ac>
2023-11-12 18:52:10 +0100

    make libzip build too todo: specify cmake find_library to check the paths we install to

--- BuildLibZip.ps1
+++ BuildLibZip.ps1
@@ -1,5 +1,6 @@
 $gModDir = (Get-Location).Path
 Write-Host Set current directory to $gModDir
+rm -r -fo build
 cmake -B build
 cd build/_deps/zlib-src
 cmake -A Win32 -DCMAKE_INSTALL_PREFIX="$($gModDir)\bin\zlib" .
@@ -7,5 +8,8 @@ cmake --build . --config Release
 cmake --install . --config Release
 cd $gModDir
 cd build/_deps/libzip-src
-cmake -DZLIB_LIBRARY="$($gModDir)\bin\zlib\lib\zlibstaticd.lib" -DZLIB_INCLUDE_DIR="$($gModDir)\bin\zlib\include" .
+cmake -A Win32 -DZLIB_LIBRARY="$($gModDir)\bin\zlib\lib\zlibstatic.lib" -DZLIB_INCLUDE_DIR="$($gModDir)\bin\zlib\include" -DCMAKE_INSTALL_PREFIX="$($gModDir)\bin\libzip" . .
+cmake --build . --config Release
+cmake --install . --config Release
 cd $gModDir
+cmake -B build
