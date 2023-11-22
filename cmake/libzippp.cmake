2ad8e91caf53fb604d29c470755491cca77982b0
DubbleClick <m@pyc.ac>
2023-11-22 21:23:35 +0100

    revert libzippp.cmake

--- cmake/libzippp.cmake
+++ cmake/libzippp.cmake
@@ -2,6 +2,15 @@ include_guard()
 include(FetchContent)
 include(libzip)
 
+find_library(ZLIB_LIBRARY REQUIRED
+    NAMES zlibstatic
+    PATHS ${CMAKE_INSTALL_PREFIX}/lib
+)
+find_library(LIBZIP_LIBRARY REQUIRED
+    NAMES zip
+    PATHS ${CMAKE_INSTALL_PREFIX}/lib
+)
+
 FetchContent_Declare(
     libzippp
     GIT_REPOSITORY https://github.com/ctabin/libzippp
@@ -12,14 +21,20 @@ if (NOT libzippp_POPULATED)
     FetchContent_Populate(libzippp)
 endif()
 
-option(CMAKE_PREFIX_PATH "" ${CMAKE_INSTALL_PREFIX})
-set(CMAKE_PREFIX_PATH ${CMAKE_INSTALL_PREFIX})
-option(LIBZIPPP_WITH_ENCRYPTION "" ON)
-set(LIBZIPPP_WITH_ENCRYPTION ON)
-option(BUILD_SHARED_LIBS "" OFF)
-set(BUILD_SHARED_LIBS OFF)
+set(SOURCES
+    "${libzippp_SOURCE_DIR}/src/libzippp.h"
+    "${libzippp_SOURCE_DIR}/src/libzippp.cpp"
+    )
+source_group(TREE ${libzippp_SOURCE_DIR} FILES ${SOURCES})
+target_sources(libzippp PRIVATE ${SOURCES})
+target_include_directories(libzippp PUBLIC "${libzippp_SOURCE_DIR}/src")
+target_include_directories(libzippp PRIVATE "${CMAKE_INSTALL_PREFIX}/include")
+target_compile_definitions(libzippp PUBLIC
+    LIBZIPPP_WITH_ENCRYPTION
+    )
+target_link_libraries(libzippp PRIVATE
+    ${LIBZIP_LIBRARY}
+    ${ZLIB_LIBRARY}
+    )
 
-add_subdirectory(${libzippp_SOURCE_DIR} EXCLUDE_FROM_ALL)
-
-target_compile_definitions(libzippp PRIVATE LIBZIPPP_WITH_ENCRYPTION=true)
 set_target_properties(libzippp PROPERTIES FOLDER "Dependencies/")
