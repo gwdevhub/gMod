852deeea0852769de3ae5a277e543fbb6b05a5cc
Jon <jon.r+gh@live.com>
2023-11-26 21:47:30 +0000

    Hook into GetProcAddress instead of d3d9 dll

--- source/dll_main.cpp
+++ source/dll_main.cpp
@@ -9,35 +9,42 @@
 
 namespace {
 
+#define DISABLE_HOOK(var) if(var) { MH_DisableHook(var); var = nullptr;}
+
     using Direct3DCreate9_type = IDirect3D9* (APIENTRY*)(UINT);
     using Direct3DCreate9Ex_type = HRESULT(APIENTRY*)(UINT SDKVersion, IDirect3D9Ex** ppD3D);
+    using GetProcAddress_type = FARPROC(APIENTRY*)(HMODULE, LPCSTR);
 
     // Pointer to original address of Direct3DCreate9
-    Direct3DCreate9_type Direct3DCreate9_fn = nullptr;
     Direct3DCreate9_type Direct3DCreate9_ret = nullptr;
 
     // Pointer to original address of Direct3DCreate9
-    Direct3DCreate9Ex_type Direct3DCreate9Ex_fn = nullptr;
     Direct3DCreate9Ex_type Direct3DCreate9Ex_ret = nullptr;
+
+    GetProcAddress_type GetProcAddress_fn = nullptr;
+    GetProcAddress_type GetProcAddress_ret = nullptr;
+
+    FARPROC APIENTRY OnGetProcAddress(HMODULE hModule, LPCSTR lpProcName) {
+        ASSERT(GetProcAddress_ret);
+        if ((int)lpProcName < 0xffff)
+            return GetProcAddress_ret(hModule, lpProcName); // lpProcName is ordinal offset, not string
+
+        if (strcmp(lpProcName, "Direct3DCreate9") == 0) {
+            Direct3DCreate9_ret = (Direct3DCreate9_type)GetProcAddress_ret(hModule, lpProcName);
+            return (FARPROC)Direct3DCreate9;
+        }
+        if (strcmp(lpProcName, "Direct3DCreate9Ex") == 0) {
+            Direct3DCreate9Ex_ret = (Direct3DCreate9Ex_type)GetProcAddress_ret(hModule, lpProcName);
+            return (FARPROC)Direct3DCreate9Ex;
+        }
+        return GetProcAddress_ret(hModule, lpProcName);
+    }
 
 
     FILE* stdout_proxy;
     FILE* stderr_proxy;
 
-    // If not nullptr, we're responsible for freeing this library on termination
-    HMODULE gMod_Loaded_d3d9_Module_Handle = nullptr;
-
-    // If this hModule called d3d9.dll?
-    bool IsD3d9Dll(HMODULE hModule)
-    {
-        TCHAR szModuleName[MAX_PATH];
-        ASSERT(GetModuleFileName(hModule, szModuleName, sizeof(szModuleName) / sizeof(*szModuleName)) > 0);
-        const auto basename = strrchr(szModuleName, '\\');
-        return basename && strcmp(basename + 1, "d3d9.dll") == 0;
-    }
-
-    HMODULE FindLoadedDll()
-    {
+    HMODULE FindLoadedModuleByName(const char* name) {
         HMODULE hModules[1024];
         HANDLE hProcess;
         DWORD cbNeeded;
@@ -50,9 +57,11 @@ namespace {
         for (i = 0; i < (cbNeeded / sizeof(HMODULE)); i++) {
             if (hModules[i] == gl_hThisInstance)
                 continue;
-            if (IsD3d9Dll(hModules[i])) {
+            TCHAR szModuleName[MAX_PATH];
+            ASSERT(GetModuleFileName(hModules[i], szModuleName, _countof(szModuleName)) > 0);
+            const auto basename = strrchr(szModuleName, '\\');
+            if (basename && stricmp(basename + 1, name) == 0)
                 return hModules[i];
-            }
         }
         return nullptr;
     }
@@ -63,6 +72,8 @@ HINSTANCE gl_hThisInstance = nullptr;
 
 IDirect3D9* APIENTRY Direct3DCreate9(UINT SDKVersion)
 {
+    DISABLE_HOOK(GetProcAddress_fn);
+
     Message("uMod_Direct3DCreate9:  original %p, uMod %p\n", Direct3DCreate9_fn, Direct3DCreate9);
 
     ASSERT(Direct3DCreate9_ret);
@@ -75,6 +86,8 @@ IDirect3D9* APIENTRY Direct3DCreate9(UINT SDKVersion)
 
 HRESULT APIENTRY Direct3DCreate9Ex(UINT SDKVersion, IDirect3D9Ex** ppD3D)
 {
+    DISABLE_HOOK(GetProcAddress_fn);
+
     Message("uMod_Direct3DCreate9Ex:  original %p, uMod %p\n", Direct3DCreate9Ex_fn, Direct3DCreate9Ex);
 
     ASSERT(Direct3DCreate9Ex_ret);
@@ -128,42 +141,26 @@ void InitInstance(HINSTANCE hModule)
     // Store the handle to this module
     gl_hThisInstance = hModule;
 
-    const auto d3d9_dll = LoadOriginalDll();
-    ASSERT(d3d9_dll);
+    // d3d9.dll shouldn't be loaded at this point.
+    const auto d3d9_loaded = FindLoadedModuleByName("d3d9.dll");
+    ASSERT(!d3d9_loaded);
 
     MH_Initialize();
 
-    // Hook our loaded Dll's dx9 calls though to uMod's functions
-    Direct3DCreate9_fn = reinterpret_cast<Direct3DCreate9_type>(GetProcAddress(d3d9_dll, "Direct3DCreate9"));
-    ASSERT(Direct3DCreate9_fn);
-    if (Direct3DCreate9_fn) {
-        MH_CreateHook(Direct3DCreate9_fn, Direct3DCreate9, (void**)&Direct3DCreate9_ret);
-        MH_EnableHook(Direct3DCreate9_fn);
+    // Hook into LoadLibraryA - we'll do our hooks on the flip side
+    GetProcAddress_fn = reinterpret_cast<GetProcAddress_type>(GetProcAddress);
+    ASSERT(GetProcAddress_fn);
+    if (GetProcAddress_fn) {
+        MH_CreateHook(GetProcAddress_fn, OnGetProcAddress, (void**)&GetProcAddress_ret);
+        MH_EnableHook(GetProcAddress_fn);
     }
-
-    Direct3DCreate9Ex_fn = reinterpret_cast<Direct3DCreate9Ex_type>(GetProcAddress(d3d9_dll, "Direct3DCreate9Ex"));
-    ASSERT(Direct3DCreate9Ex_fn);
-    if (Direct3DCreate9Ex_fn) {
-        MH_CreateHook(Direct3DCreate9Ex_fn, Direct3DCreate9Ex, (void**)&Direct3DCreate9Ex_ret);
-        MH_EnableHook(Direct3DCreate9Ex_fn);
-    }
-
 }
 
 void ExitInstance()
 {
-    if (Direct3DCreate9_fn)
-        MH_DisableHook(Direct3DCreate9_fn);
-    if (Direct3DCreate9Ex_fn)
-        MH_DisableHook(Direct3DCreate9Ex_fn);
+    DISABLE_HOOK(GetProcAddress_fn);
 
     MH_Uninitialize();
-
-    // Release the system's d3d9.dll
-    if (gMod_Loaded_d3d9_Module_Handle != nullptr) {
-        ASSERT(FreeLibrary(gMod_Loaded_d3d9_Module_Handle));
-        gMod_Loaded_d3d9_Module_Handle = nullptr;
-    }
 
 #ifdef _DEBUG
     if (stdout_proxy)
@@ -176,45 +173,3 @@ void ExitInstance()
     __except (EXCEPTION_CONTINUE_EXECUTION) { }
 #endif
 }
-
-HMODULE LoadOriginalDll()
-{
-    HMODULE found = FindLoadedDll();
-    if (found)
-        return found;
-
-    char executable_path[MAX_PATH]{};
-    ASSERT(GetModuleFileName(GetModuleHandle(nullptr), executable_path, _countof(executable_path)) > 0);
-
-    char dll_path[MAX_PATH]{};
-    ASSERT(GetModuleFileName(gl_hThisInstance, dll_path, _countof(dll_path)) > 0);
-
-    const auto exe_path = std::filesystem::path(executable_path);
-    const auto gmod_path = std::filesystem::path(dll_path);
-
-    if (exe_path.parent_path() != gmod_path.parent_path()
-        || gmod_path.filename() != "d3d9.dll") {
-        // Call basic LoadLibrary function; we're not in the same directory as the exe.
-        gMod_Loaded_d3d9_Module_Handle = LoadLibrary("d3d9.dll");
-    }
-    else {
-        // We're in the same directory as the exe, and we're called 'd3d9.dll'. Calling vanilla "LoadLibrary" will be recursive!
-        char buffer[MAX_PATH];
-        ASSERT(GetSystemDirectory(buffer, _countof(buffer) > 0)); //get the system directory, we need to open the original d3d9.dll
-
-        // Append dll name
-        strcat_s(buffer, _countof(buffer), "\\d3d9.dll");
-        gMod_Loaded_d3d9_Module_Handle = LoadLibrary(buffer);
-    }
-
-    ASSERT(gMod_Loaded_d3d9_Module_Handle);
-
-    found = FindLoadedDll();
-    ASSERT(found && found == gMod_Loaded_d3d9_Module_Handle);
-
-    return found;
-}
-
-/*
- * We inject the dll into the game, thus we retour the original Direct3DCreate9 function to our MyDirect3DCreate9 function
- */
