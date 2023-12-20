21c20c80e5fbee483dd4584acd94942ae70674da
DubbleClick <m@pyc.ac>
2023-12-20 16:48:15 +0100

    fix unicode characters like Umlaute üäÜÖßéè

--- header/Utils.h
+++ header/Utils.h
@@ -12,4 +12,17 @@ namespace utils
             vec.erase(found);
         }
     }
+
+    inline std::wstring utf8_to_wstring(const std::string& utf8str) {
+        if (utf8str.empty()) return {};
+
+        // Calculate the number of wide characters needed for the conversion
+        const int count = MultiByteToWideChar(CP_UTF8, 0, utf8str.c_str(), -1, nullptr, 0);
+        if (count == 0) throw std::runtime_error("Failed to convert UTF-8 to UTF-16");
+
+        std::wstring wstr(count, 0);
+        MultiByteToWideChar(CP_UTF8, 0, utf8str.c_str(), -1, wstr.data(), count);
+
+        return wstr;
+    }
 }
